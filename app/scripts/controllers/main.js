6231b6070d0a0a39d1b288b4d758ba114ac16207
Tom DeVito <tom.devito22@gmail.com>
2014-02-24 14:21:00 -0500

    Fixed failing biuild

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,15 +1,15 @@
 'use strict';
 
 angular.module('barterApp')
-  .controller('MainCtrl', function (ListingService, $scope) {
+  .controller('MainCtrl', function ($scope, ListingService) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
     ListingService.getListings().then(function(data) {
-    	$scope.listings = data.results;
-    	console.log($scope.listings);
+      $scope.listings = data.results;
+      console.log($scope.listings);
     });
 //    console.log($scope.listings);
   })
