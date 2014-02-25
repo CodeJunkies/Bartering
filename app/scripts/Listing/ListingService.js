58de0c2a4f7b25ef7fc94f5af751ce670452f60f
Tom DeVito <tom.devito22@gmail.com>
2014-02-24 22:27:47 -0500

    Removed setting the headers per request via listing service.  This will happen by default by the AuthModule.

--- app/scripts/Listing/ListingService.js
+++ app/scripts/Listing/ListingService.js
@@ -5,16 +5,8 @@ angular.module('barterApp')
     var ListingService = {
       getListings: function() {
 
-        var config = {
-          headers: {
-            // Where do we keep these?
-            'X-Parse-Application-Id': 'yCqv3UTcDTe6a3l00WaRT1Xt9FL7Wb7eaXRirbsV',
-            'X-Parse-REST-API-Key': 'lTdx32u6TIttb6wUZWhdOwF13WH912NfkXVZw7Ym',
-          }
-        };
-
         // $http returns a promise, which has a then function, which also returns a promise
-        var promise = $http.get('https://api.parse.com/1/classes/Listing', config).then(function (response) {
+        var promise = $http.get('https://api.parse.com/1/classes/Listing').then(function (response) {
 
           // The return value gets picked up by the then in the controller.
           return response.data;
