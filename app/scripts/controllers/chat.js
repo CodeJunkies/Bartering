4d406e9553d5030dd7f0beab37c1ae3a0993b3c9
gudnm <srochno@gmail.com>
2014-03-02 17:54:31 -0500

    Add some methods to Chat controller and comment out path to mocks in karma config

--- app/scripts/controllers/chat.js
+++ app/scripts/controllers/chat.js
@@ -3,6 +3,15 @@
 angular.module('barterApp')
   .controller('ChatCtrl', function($scope, ChatService) {
     
+  	var createChat = function(newChat) {
+  	  newChat.$save();
+  	  $scope.chats.push(newChat);
+  	}
+
+  	var updateChat = function(chat) {
+  	  chat.$update();
+  	}
+
     var chats = ChatService.query(function() {
       $scope.chats = chats.results;
     });
